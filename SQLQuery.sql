258cc3ad34ab8a458c497399935092c0cc3e115f
PavanManiKanta48 <pavanmk@moback.com>
2022-11-01 15:32:58 +0530

    C# Report

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -13,6 +13,47 @@ select*from DimProduct
  from DimEmployee
  join DimProduct
  on DimEmployee.EmployeeKey=DimProduct.ProductKey;
+ -----Except clause----
+ CREATE Database BookStore;
+GO
+USE BookStore;
+ 
+CREATE TABLE Books1
+(
+    id INT primary key,
+    name VARCHAR(50) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price INT NOT NULL
+ )
+ 
+CREATE TABLE Books2
+(
+    id INT primary key,
+    name VARCHAR(50) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price INT NOT NULL
+ )
+ insert into books2
+ values
+ (11, 'Book11', 'Cat11', 1800),
+(12, 'Book12', 'Cat12', 1500),
+(13, 'Book13', 'Cat13', 2000),
+(14, 'Book14', 'Cat14', 1300),
+(15, 'Book15', 'Cat15', 1500),
+(6, 'Book6', 'Cat6', 5000),
+(7, 'Book7', 'Cat7', 8000),
+(8, 'Book8', 'Cat8', 5000),
+(9, 'Book9', 'Cat9', 5400),
+(10, 'Book10', 'Cat10', 3200)
+
+ select * from Books2
+ except
+ select * from Books2 where price>5000
+
+ select * from books1
+ where id not in (select id from Books2)
+  
+
 
  --create New Tables for joining --
 
@@ -100,4 +141,53 @@ select*from DimProduct
   from Employee
   full join Department
   on Employee.Deptid=Department.Did  
+
+  select * from Employee
+ select * from Department
+ --used Between Operator to delete multiple rows
+delete from Department where Did between 106 and 112
+--inner joins
+select e.EName,e.esal,e.Deptid,d.Did,d.Dname
+from Employee as e
+ join Department as d on e.Deptid=d.Did
+
+ --inner join
+ select a.ename as Employee,b.ename as manager 
+ from Employee a
+ cross join employee b 
+
+  select * from Employee
+  select a.ename as employee,b.ename as manager
+  from Employee a,Employee b
+  where a.Eid=b.Deptid
   
+  --guid
+
+  create table Employee
+  ( 
+  EmpId uniqueidentifier primary key default NewId(),
+  EName varchar(50),
+  Esal money,
+  [Address] varchar(50)
+  )
+
+  insert into Employee(EName,Esal,[Address])
+  values
+  ('pavan',15000,'bhimavaram'),
+  ('ram',15000,'palakollu'),
+  ('bheem',15000,'visakhapatnam'),
+  ('tarun',15000,'machilipatnam')
+
+  select * from Employee where 
+
+  create procedure sp_Employee
+  as
+  begin
+  select * from Employee
+  end
+
+  helptext employee
+
+ 
+
+ sp_Employee
